b2a165796cae30d33bc82e8c2213e4ee7ed14f0d
Jake Dowling <jacobdowling07@gmail.com>
2023-03-02 20:47:58 +0000

    yes

---
+++ ptp-depend.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo ";$nrconf{restart} = 'a';" >> /etc/needrestart/needrestart.conf
+apt -y full-upgrade
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+apt -y update
+apt-add-repository --force-yes -y universe
+apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip}  nginx tar unzip git redis-server
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+mkdir -p /var/www/pterodactyl
+cd /var/www/pterodactyl
+curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+tar -xzvf panel.tar.gz
+chmod -R 755 storage/* bootstrap/cache/
