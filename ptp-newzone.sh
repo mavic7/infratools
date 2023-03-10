509bc822c15088bcbea3e2dd178abc6b62688896
Jake <65215994+mavic7@users.noreply.github.com>
2023-03-10 15:20:01 +0000

    Update ptp-newzone.sh

--- ptp-newzone.sh
+++ ptp-newzone.sh
@@ -18,7 +18,7 @@ apt -y install software-properties-common curl apt-transport-https ca-certificat
 LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
 
 # Add Redis official APT repository
-curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 
 # MariaDB repo setup script can be skipped on Ubuntu 22.04
