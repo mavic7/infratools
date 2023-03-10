ab51329bddb20bc00809e27eb75e5bce50a9e711
Jake <65215994+mavic7@users.noreply.github.com>
2023-03-10 15:31:11 +0000

    Update ptp-newzone.sh

--- ptp-newzone.sh
+++ ptp-newzone.sh
@@ -28,7 +28,6 @@ curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 apt update
 apt-add-repository --force-yes universe
 apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} nginx tar unzip git redis-server mariadb-server
-systemctl stop mariadb.service
 
 mysql --user=root <<_EOF_
 UPDATE mysql.user SET Password=PASSWORD('$dbpwd') WHERE User='root';
@@ -38,6 +37,7 @@ DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 _EOF_
+systemctl stop mariadb.service
 echo -e "[mysqld]\nbinlog_format=ROW\ndefault-storage-engine=innodb\ninnodb_autoinc_lock_mode=2\nbind_address=0.0.0.0\nwsrep_on=ON\nwsrep_provider = /var/lib/galera/libgalera_smm.so\nwsrep_cluster_name=\"$galeraname\"\nwsrep_cluster_address=\"$gcommaddr\"\nwsrep_sst_method=rsync\nwsrep_node_address=\"$nodeaddr\"\nwsrep_node_name=\"$hostname\"\n" > /etc/mysql/conf.d/galera.cnf
 
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
@@ -62,7 +62,7 @@ php artisan migrate --seed --force
 php artisan p:user:make
 chown -R www-data:www-data /var/www/pterodactyl/*
 echo "* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1" >> /etc/crontab
-echo -e "[Unit]\nDescription=Pterodactyl Queue Worker\nAfter=redis-server.service\n[Service]\nUser=www-data\nGroup=www-data\nRestart=always\nExecStart=/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3\nStartLimitInterval=180\nStartLimitBurst=30\nRestartSec=5s\n\n[Install]\nWantedBy=multi-user.target" >> /etc/systemd/pteroq.service
+echo -e "[Unit]\nDescription=Pterodactyl Queue Worker\nAfter=redis-server.service\n[Service]\nUser=www-data\nGroup=www-data\nRestart=always\nExecStart=/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3\nStartLimitInterval=180\nStartLimitBurst=30\nRestartSec=5s\n\n[Install]\nWantedBy=multi-user.target" >> /etc/systemd/pteroq.conf
 systemctl enable --now redis-server
 systemctl enable --now pteroq.service
 rm /etc/nginx/sites-enabled/default
