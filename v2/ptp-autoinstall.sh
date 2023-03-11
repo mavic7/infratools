2b89a26c5c34deab9898005630a0349ee2f9160a
Jake <65215994+mavic7@users.noreply.github.com>
2023-03-11 07:31:09 +0000

    done?

--- v2/ptp-autoinstall.sh
+++ v2/ptp-autoinstall.sh
@@ -46,69 +46,7 @@ systemctl enable --now redis-server
 systemctl enable --now pteroq.service
 rm /etc/nginx/sites-enabled/default
 
-echo -e "server_tokens off;
 
-server {
-    listen 80;
-    server_name $panelurl;
-    return 301 https://\$server_name\$request_uri;
-}
-
-server {
-    listen 443 ssl http2;
-    server_name $panelurl;
-
-    root /var/www/pterodactyl/public;
-    index index.php;
-
-    access_log /var/log/nginx/pterodactyl.app-access.log;
-    error_log  /var/log/nginx/pterodactyl.app-error.log error;
-
-    # allow larger file uploads and longer script runtimes
-    client_max_body_size 100m;
-    client_body_timeout 120s;
-
-    sendfile off;
-
-    ssl_certificate /etc/letsencrypt/live/$panelurl/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$panelurl/privkey.pem;
-    ssl_session_cache shared:SSL:10m;
-    ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_ciphers \"ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384\";
-    ssl_prefer_server_ciphers on;
-
-    # See https://hstspreload.org/ before uncommenting the line below.
-    # add_header Strict-Transport-Security \"max-age=15768000; preload;\";
-    add_header X-Content-Type-Options nosniff;
-    add_header X-XSS-Protection \"1; mode=block\";
-    add_header X-Robots-Tag none;
-    add_header Content-Security-Policy \"frame-ancestors 'self'\";
-    add_header X-Frame-Options DENY;
-    add_header Referrer-Policy same-origin;
-
-    location / {
-        try_files \$uri \$uri/ /index.php?\$query_string;
-    }
-
-    location ~ \.php$ {
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/run/php/php8.1-fpm.sock;
-        fastcgi_index index.php;
-        include fastcgi_params;
-        fastcgi_param PHP_VALUE \"upload_max_filesize = 100M \n post_max_size=100M\";
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        fastcgi_param HTTP_PROXY "";
-        fastcgi_intercept_errors off;
-        fastcgi_buffer_size 16k;
-        fastcgi_buffers 4 16k;
-        fastcgi_connect_timeout 300;
-        fastcgi_send_timeout 300;
-        fastcgi_read_timeout 300;
-        include /etc/nginx/fastcgi_params;
-    }
-
-    location ~ /\.ht {
-        deny all;
-    }
-}"
-echo "PLEASE CONFIGURE NGINX MANUALLY: https://pterodactyl.io/panel/1.0/webserver_configuration.html"
+echo -e "server_tokens off;\nserver {\n    listen 80;\n    server_name $panelurl;\n    return 301 https://\$server_name\$request_uri;\n}\nserver {\n    listen 443 ssl http2;\n    server_name $panelurl;\n    root /var/www/pterodactyl/public;\n    index index.php;\n    access_log /var/log/nginx/pterodactyl.app-access.log;\n    error_log  /var/log/nginx/pterodactyl.app-error.log error;\n        client_max_body_size 100m;\n    client_body_timeout 120s;\n    sendfile off;\n    ssl_certificate /etc/letsencrypt/live/$panelurl/fullchain.pem;\n    ssl_certificate_key /etc/letsencrypt/live/$panelurl/privkey.pem;\n    ssl_session_cache shared:SSL:10m;\n    ssl_protocols TLSv1.2 TLSv1.3;\n    ssl_ciphers \"ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384\";\n    ssl_prefer_server_ciphers on;\n    # See https://hstspreload.org/ before uncommenting the line below.\n    # add_header Strict-Transport-Security \"max-age=15768000; preload;\";\n    add_header X-Content-Type-Options nosniff;\n    add_header X-XSS-Protection \"1; mode=block\";\n    add_header X-Robots-Tag none;\n    add_header Content-Security-Policy \"frame-ancestors 'self'\";\n    add_header X-Frame-Options DENY;\n    add_header Referrer-Policy same-origin;\n    location / {\n        try_files \$uri \$uri/ /index.php?\$query_string;\n    }\n    location ~ \.php$ {\n        fastcgi_split_path_info ^(.+\.php)(/.+)$;\n        fastcgi_pass unix:/run/php/php8.1-fpm.sock;\n        fastcgi_index index.php;\n        include fastcgi_params;\n        fastcgi_param PHP_VALUE \"upload_max_filesize = 100M \n post_max_size=100M\";\n        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;\n        fastcgi_param HTTP_PROXY "";\n        fastcgi_intercept_errors off;\n        fastcgi_buffer_size 16k;\n        fastcgi_buffers 4 16k;\n        fastcgi_connect_timeout 300;\n        fastcgi_send_timeout 300;\n        fastcgi_read_timeout 300;\n        include /etc/nginx/fastcgi_params;\n    }\n    location ~ /\.ht {\n        deny all;\n    }\n}" > /etc/nginx/sitess-available/pterodactyl.conf
+ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
+echo "Fully completed."
